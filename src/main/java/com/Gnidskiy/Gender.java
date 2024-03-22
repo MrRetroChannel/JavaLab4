a709916af697655000504090203f73dceea05f26
MrRetro <gleb2super@gmail.com>
2024-03-22 23:17:57 +0300

    Added data classes

---
+++ src/main/java/com/Gnidskiy/Gender.java
@@ -0,0 +1,8 @@
+package com.Gnidskiy;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    BATTLE_HELICOPTER,
+    SOMETHING_ELSE_IDK
+}
---
+++ src/main/java/com/Gnidskiy/Person.java
@@ -0,0 +1,65 @@
+package com.Gnidskiy;
+
+import java.util.Date;
+
+public class Person {
+    private int id;
+
+    private String name;
+
+    private Gender gender;
+
+    private Division division;
+
+    private double salary;
+
+    private Date birthDay;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Division getDivision() {
+        return division;
+    }
+
+    public void setDivision(Division division) {
+        this.division = division;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+}
